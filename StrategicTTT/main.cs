f821f4fe681d72c345bd1293a81069efd4e2e9a0
earthToAlice <girlofthedoodles@gmail.com>
2019-11-16 11:44:24 -0500

    Add project files.

---
+++ StrategicTTT/main.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StrategicTTT
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
